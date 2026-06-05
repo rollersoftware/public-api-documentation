1fb4bad21094bee5ce27879ae914043c129e33e8
alistair.edge@rollerdigital.com <alistair.edge@rollerdigital.com>
2026-06-05 15:29:57 +1000

    Modified 5 files
    Committed by alistair-edge on behalf of alistair.edge@rollerdigital.com

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMxMzkxOA/spectral.js?branch_slug=main&token=d3956e01-d6a2-48e6-b44a-2e1cb6f14774";
+export default { extends: ruleset };
